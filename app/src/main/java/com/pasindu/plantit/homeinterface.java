438a29159da810c6e662ef45fba6ee75053a42d6
Gihan pasidu <sahanrowdy@gmail.com>
2025-06-21 11:06:07 +0530

    update

--- app/src/main/java/com/pasindu/plantit/homeinterface.java
+++ app/src/main/java/com/pasindu/plantit/homeinterface.java
@@ -12,48 +12,78 @@ public class homeinterface extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homeinterface);
 
-        Button btnProfile = findViewById(R.id.btnProfile);
-        Button btnPlantRegistration = findViewById(R.id.btnPlantRegistration);
-        Button btnDiseaseDetection = findViewById(R.id.btnDiseaseDetection);
-        Button btnSensorMonitoring = findViewById(R.id.btnSensorMonitoring);
-        Button btnNavigation = findViewById(R.id.btnNavigation);
+        // View My Plants -> Plant Profile Screen
+        Button btnViewMyPlants = findViewById(R.id.btnViewMyPlants);
+        btnViewMyPlants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement PlantListActivity
+                startActivity(new Intent(homeinterface.this, PlantListActivity.class));
+            }
+        });
+
+        // Add New Plant -> Plant Registration
+        Button btnAddNewPlant = findViewById(R.id.btnAddNewPlant);
+        btnAddNewPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement PlantRegistrationActivity
+                startActivity(new Intent(homeinterface.this, PlantRegistrationActivity.class));
+            }
+        });
+
+        // Disease Diagnosis -> Disease Diagnosis Screen
+        Button btnDiseaseDiagnosis = findViewById(R.id.btnDiseaseDiagnosis);
+        btnDiseaseDiagnosis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement DiseaseDiagnosisActivity
+                startActivity(new Intent(homeinterface.this, DiseaseDiagnosisActivity.class));
+            }
+        });
+
+        // Care Schedule -> Care Schedule Screen
+        Button btnCareSchedule = findViewById(R.id.btnCareSchedule);
+        btnCareSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement CareScheduleActivity
+                startActivity(new Intent(homeinterface.this, CareScheduleActivity.class));
+            }
+        });
+
+        // Environmental Monitoring -> Environmental Monitoring Screen
+        Button btnEnvironmentalMonitoring = findViewById(R.id.btnEnvironmentalMonitoring);
+        btnEnvironmentalMonitoring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement EnvironmentalMonitoringActivity
+                startActivity(new Intent(homeinterface.this, EnvironmentalMonitoringActivity.class));
+            }
+        });
+
+        // Nearby Services -> Nearby Services (Google Map)
+        Button btnNearbyServices = findViewById(R.id.btnNearbyServices);
+        btnNearbyServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement NearbyServicesActivity
+                startActivity(new Intent(homeinterface.this, NearbyServicesActivity.class));
+            }
+        });
+
+        // Settings -> Settings Screen
+        Button btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement SettingsActivity
+                startActivity(new Intent(homeinterface.this, SettingsActivity.class));
+            }
+        });
+
+        // Logout
         Button btnLogout = findViewById(R.id.btnLogout);
-
-        btnProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homeinterface.this, userprofile.class));
-            }
-        });
-
-        btnPlantRegistration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homeinterface.this, plantprofile.class));
-            }
-        });
-
-        btnDiseaseDetection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homeinterface.this, diseasedetection.class));
-            }
-        });
-
-        btnSensorMonitoring.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homeinterface.this, sensormonitoring.class));
-            }
-        });
-
-        btnNavigation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(homeinterface.this, navigation.class));
-            }
-        });
-
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
